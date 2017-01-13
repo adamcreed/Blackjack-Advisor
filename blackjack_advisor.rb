1a559a0c4633452b6d2d5a53466d8874a998b159
Adam Reed <kusa.xisa@gmail.com>
2017-01-13 00:56:06 -0500

    Added lookup and partial tables

--- blackjack_advisor.rb
+++ blackjack_advisor.rb
@@ -7,12 +7,35 @@ def is_valid_hand?(cards, expected)
   count == expected
 end
 
+def clean_input(cards)
+  i = 0
+  formatted_hand = []
+
+  until cards[i] == nil
+    if cards[i].upcase =~ /[AKQJ2-9]/
+      add_card(cards[i], formatted_hand)
+
+    elsif cards[i] == '1' and cards[i + 1] == '0'
+      add_card('10', formatted_hand)
+
+    end
+
+    i += 1
+  end
+
+  formatted_hand
+end
+
+def add_card(card, hand)
+  hand << card
+end
+
 def are_cards?(cards)
   is_a_card = true
   previous = ''
   cards.split("").each do |card|
     if is_a_card
-      is_a_card = card.downcase =~ /[akqj1-9 ]/
+      is_a_card = card.upcase =~ /[AKQJ1-9 ]/
       is_a_card = true if previous == '1' and card == '0'
       is_a_card = false if previous == '1' and not (card == '0')
       previous = card
@@ -55,16 +78,87 @@ def singular_or_plural(quantity, noun)
   end
 end
 
-hard_hand = {}
-soft_hand = {}
-pair = {}
+def get_user_hand
+  print 'Enter your two cards: '
+  until is_valid_hand?(hand = gets.chomp, 2); end
+  #This seems kinda weird, but I don't actully need the loop to do anything except pass
+  hand
+end
 
-print 'Enter your two cards: '
-until is_valid_hand?(hand = gets.chomp, 2)
+def get_dealer_card
+  print "Enter the dealer's card: "
+  until is_valid_hand?(dealer_card = gets.chomp, 1); end
+  dealer_card
+end
+
+def find_hand_type(hand)
+  if hand[0] == hand[1]
+    'a_pair'
+
+  elsif hand.include?('A')
+    'soft_hand'
+
+  else
+    'hard_hand'
+
+  end
+end
+
+def find_value(hand)
+  hand.each do |card|
+    if card.upcase =~ /KQJ/
+      card = '10'
+
+    elsif card.upcase == 'A'
+      card = '11'
+
+    end
+  end
+
+  if hand.size == 2
+    hand[0] + hand[1]
+  else
+    hand[0]
+  end
 
 end
 
-print "Enter the dealer's card: "
-until is_valid_hand?(dealer = gets.chomp, 1)
+def get_suggestion(hand_type, hand_value, dealer_card)
+  hard_hand = { 5 => {2 => 'Hit', 3 => 'Hit', 4 => 'Hit', 5 => 'Hit',
+                      6 => 'Hit', 7 => 'Hit', 8 => 'Hit', 9 => 'Hit',
+                      10 => 'Hit', 'A' => 'Hit'},
+                6 => {2 => 'Hit', 3 => 'Hit', 4 => 'Hit', 5 => 'Hit',
+                      6 => 'Hit', 7 => 'Hit', 8 => 'Hit', 9 => 'Hit',
+                      10 => 'Hit', 'A' => 'Hit'},
+                7 => {2 => 'Hit', 3 => 'Hit', 4 => 'Hit', 5 => 'Hit',
+                       6 => 'Hit', 7 => 'Hit', 8 => 'Hit', 9 => 'Hit',
+                       10 => 'Hit', 'A' => 'Hit'},
+                8 => {2 => 'Hit', 3 => 'Hit', 4 => 'Hit',
+                      5 => 'Double if possible, otherwise Hit',
+                      6 => 'Double if possible, otherwise Hit', 7 => 'Hit',
+                      8 => 'Hit', 9 => 'Hit', 10 => 'Hit', 'A' => 'Hit'}}
+  soft_hand = {}
+  a_pair = {}
 
+  case hand_type
+  when 'hard_hand'
+    hard_hand{hand_value{dealer_card}}
+
+  when 'soft_hand'
+    soft_hand{hand_value{dealer_card}}
+
+  when 'pair'
+    a_pair{hand_value{dealer_card}}
+
+  end
 end
+
+hand = get_user_hand
+dealer_card = get_dealer_card
+
+hand = clean_input(hand)
+dealer_card = clean_input(dealer_card)
+hand_type = find_hand_type(hand)
+hand_value = find_value(hand)
+
+puts 'You should ' + get_suggestion(hand_type, hand_value, dealer_card).to_s
